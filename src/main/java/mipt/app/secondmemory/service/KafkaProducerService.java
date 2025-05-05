35385e4cd46edb4a0fa8fdb95031d386d973a622
Samyrai47 <sivolobov.ilya2@gmail.com>
2025-05-05 22:19:59 +0300

    some changes

--- src/main/java/mipt/app/secondmemory/service/KafkaProducerService.java
+++ src/main/java/mipt/app/secondmemory/service/KafkaProducerService.java
@@ -2,6 +2,8 @@ package mipt.app.secondmemory.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import lombok.extern.slf4j.Slf4j;
@@ -18,14 +20,24 @@ public class KafkaProducerService {
   private final KafkaTemplate<String, String> kafkaTemplate;
   private final ObjectMapper objectMapper;
   private final String topic;
+  private final MeterRegistry registry;
+
+  private Counter getMessagesCounter(String type) {
+    return Counter.builder("kafka.requests")
+        .description("Number of messages sent")
+        .tags("type", type)
+        .register(registry);
+  }
 
   public KafkaProducerService(
       KafkaTemplate<String, String> kafkaTemplate,
       ObjectMapper objectMapper,
-      @Value("${topic-to-send-message}") String topic) {
+      @Value("${topic-to-send-message}") String topic,
+      MeterRegistry registry) {
     this.kafkaTemplate = kafkaTemplate;
     this.objectMapper = objectMapper;
     this.topic = topic;
+    this.registry = registry;
   }
 
   public void sendMessage(MessageDto messageDto) throws JsonProcessingException {
@@ -33,6 +45,7 @@ public class KafkaProducerService {
 
     try {
       CompletableFuture<SendResult<String, String>> sendResult = kafkaTemplate.send(topic, message);
+      getMessagesCounter("sentMessage").increment();
     } catch (ExecutionException | CompletionException ex) {
       log.error("Error occurred while sending message with kafka", ex);
     }
